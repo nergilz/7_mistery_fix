8467391f766c1c5e2d8e7bd0e4febb1c942f512a
john <nergilz@yandex.ru>
2018-02-16 21:39:59 +0200

    при D<0 код возвращает None,None; иначе - проводит вычисления

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,21 +1,14 @@
 from math import sqrt
 
 
-def _root1(a, b, discriminant):
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    return root1
-
-
-def _root2(a, b, discriminant):
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    return root2
-
-
 def get_roots(a, b, c):
     discriminant = b ** 2 - (4 * a * c)
     if discriminant < 0:
         return None, None
-    elif discriminant == 0:
-        return _root1(a, b, discriminant), None
     else:
-        return _root1(a, b, discriminant), _root2(a, b, discriminant)
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        if discriminant == 0:
+            return root1, None
+        else:
+            return root1, root2
